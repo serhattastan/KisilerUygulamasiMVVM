e58331b95a5a382612a70ed2c17d6a65989b9e71
Serhat Taştan <serhattastannn@gmail.com>
2023-12-02 01:38:09 +0300

    Room Database

--- app/src/main/java/com/example/kisileruygulamasi/data/datasource/KisilerDataSource.kt
+++ app/src/main/java/com/example/kisileruygulamasi/data/datasource/KisilerDataSource.kt
@@ -2,38 +2,30 @@ package com.example.kisileruygulamasi.data.datasource
 
 import android.util.Log
 import com.example.kisileruygulamasi.data.entity.Kisiler
+import com.example.kisileruygulamasi.room.KisilerDao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class KisilerDataSource {
+class KisilerDataSource(var kdao : KisilerDao) {
 
     suspend fun kisileriYukle() : List<Kisiler> =
         withContext(Dispatchers.IO){
-            val kisilerListesi = ArrayList<Kisiler>()
-            val k1 = Kisiler(1,"Ahmet", "11111")
-            val k2 = Kisiler(1,"Mahmut", "22222")
-            val k3 = Kisiler(1,"Rıza", "33333")
-            val k4 = Kisiler(1,"Kamil", "44444")
-            kisilerListesi.add(k1)
-            kisilerListesi.add(k2)
-            kisilerListesi.add(k3)
-            kisilerListesi.add(k4)
-            return@withContext kisilerListesi
+            return@withContext kdao.kisileriYukle()
         }
     suspend fun ara(aramaKelimesi : String): List<Kisiler> =
         withContext(Dispatchers.IO){
-            val kisilerListesi = ArrayList<Kisiler>()
-            val k1 = Kisiler(1,"Ahmet", "11111")
-            kisilerListesi.add(k1)
-            return@withContext kisilerListesi
+            return@withContext kdao.ara(aramaKelimesi)
         }
     suspend fun kaydet(kisi_ad : String, kisi_tel : String){
-        Log.e("Kişi Kaydet", "$kisi_ad - $kisi_tel")
+        val yeniKisi = Kisiler(0, kisi_ad, kisi_tel)
+        kdao.kaydet(yeniKisi)
     }
     suspend fun guncelle(kisi_id : Int, kisi_ad : String, kisi_tel : String){
-        Log.e("Kişi Güncelle", "$kisi_id - $kisi_ad - $kisi_tel")
+        val guncellenenKisi = Kisiler(kisi_id, kisi_ad, kisi_tel)
+        kdao.guncelle(guncellenenKisi)
     }
     suspend fun sil(kisi_id : Int){
-        Log.e("Kişi Sil", kisi_id.toString())
+        val silinenKisi = Kisiler(kisi_id, "", "")
+        kdao.sil(silinenKisi)
     }
 }
--- app/src/main/java/com/example/kisileruygulamasi/di/AppModule.kt
+++ app/src/main/java/com/example/kisileruygulamasi/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.example.kisileruygulamasi.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.kisileruygulamasi.data.datasource.KisilerDataSource
 import com.example.kisileruygulamasi.data.entity.Kisiler
 import com.example.kisileruygulamasi.data.repo.KisilerRepository
+import com.example.kisileruygulamasi.room.KisilerDao
+import com.example.kisileruygulamasi.room.Veritabani
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,12 +19,18 @@ import javax.inject.Singleton
 class AppModule {
     @Provides
     @Singleton
-    fun provideKisilerDataSource() : KisilerDataSource{
-        return KisilerDataSource()
+    fun provideKisilerDataSource(kdao : KisilerDao) : KisilerDataSource{
+        return KisilerDataSource(kdao)
     }
     @Provides
     @Singleton
     fun provideKisilerRepository(kds : KisilerDataSource) : KisilerRepository{
         return KisilerRepository(kds)
+    }
+    @Provides
+    @Singleton
+    fun provideKisilerDao(@ApplicationContext context:Context) : KisilerDao{
+        val vt = Room.databaseBuilder(context, Veritabani::class.java, "rehber.sqlite").createFromAsset("rehber.sqlite").build()
+        return vt.getKisilerDao()
     }
 }
